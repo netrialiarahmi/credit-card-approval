1f557287859fcc3db9384085a485e6c7520ef381
Netri Alia Rahmi <86939625+netrialiarahmi@users.noreply.github.com>
2024-09-19 23:28:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ if submitted:
     reason_prompt = f"""
     Based on the following data:
     Gender: {GENDER}, Car Owner: {Car_Owner}, Property Owner: {Propert_Owner},
-    Number of Children: {CHILDREN}, Annual Income: {Annual_income}, 
+    Number of Children: {CHILDREN}, Annual Income: {Annual_income}, Type Occupation: {Type_Occupation},
     Type of Income: {Type_Income}, Education Level: {EDUCATION}, Marital Status: {Marital_status},
     Number of Family Members: {Family_Members}, Age Group: {Age}, Tenure: {Tenure_value:.2f}, Unemployment Duration: {Unemployment_duration},
     You are a credit card officer. Please provide a detailed reason why the credit card application was {'approved' if predictions[0] == 1 else 'rejected'}. Write in Bahasa Indonesia with emoticons, without a conclusion or introduction, just the reason.
