4d3cac57f7623debd9892c9cf6450cddb4e79273
Netri Alia Rahmi <86939625+netrialiarahmi@users.noreply.github.com>
2024-09-19 23:14:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ else:
     Ind_ID = 1
 
 with st.form("input_form"):
-    col1, col2 = st.columns([1, 1])
+    col1, col2,col3 = st.columns([1, 1,1])
 
     with col1:
         st.text_input("Ind ID", value=Ind_ID, disabled=True)
@@ -46,14 +46,16 @@ with st.form("input_form"):
         CHILDREN = st.number_input("Number of Children", min_value=0, value=0)
         Annual_income = st.number_input("Annual Income", value=180000.0)
         Type_Income = st.selectbox("Type of Income", options=['Commercial associate', 'State servant', 'Working', 'Pensioner'], index=3)
+
+
+    with col2:
         EDUCATION = st.selectbox("Education Level", options=['Higher education', 'Secondary / secondary special', 'Incomplete higher', 'Lower secondary'], index=0)
         Marital_status = st.selectbox("Marital Status", options=['Married', 'Single', 'Separated/Widow'], index=0)
         Family_Members = st.number_input("Family Members", min_value=1, value=1)
         Unemployment_duration = st.number_input("Unemployment Duration", min_value=0, value=0)
-
-    with col2:
         Housing_type = st.selectbox("Housing Type", options=['House / apartment', 'Co-op apartment', 'Municipal apartment', 'Office apartment', 'Rented apartment', 'With parents'], index=0)
         Birthday_count = st.number_input("Birthday Count", value=-18772.0)
+    with col3:
         Employed_days = st.number_input("Employed Days", value=365243)
         Mobile_phone = st.selectbox("Mobile Phone", options=['Y', 'N'], index=0)
         Work_Phone = st.selectbox("Work Phone", options=['Y', 'N'], index=1)
@@ -65,6 +67,7 @@ with st.form("input_form"):
             'Private service staff', 'Security staff', 'Drivers', 'Cooking staff',
             'Cleaning staff', 'Waiters/barmen staff', 'Laborers', 'Low-skill Laborers'
         ], index=1)
+        
     submitted = st.form_submit_button("Submit")
 
 if submitted:
